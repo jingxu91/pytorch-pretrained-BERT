4b7a6c752142dafb022ff63bf0bddd5dd00df8f2
Jing Xu <jing.xu@protagolabs.com>
2019-03-24 22:34:24 -0700

    add intent data

--- run_classifier.py
+++ run_classifier.py
@@ -182,7 +182,7 @@ class ColaProcessor(DataProcessor):
 
     def get_labels(self):
         """See base class."""
-        return ["-1","0", "1","2","3","4","5","6","7","8","9","10"]
+        return ["0", "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
 
     def _create_examples(self, lines, set_type):
         """Creates examples for the training and dev sets."""
@@ -436,7 +436,7 @@ def main():
     }
 
     num_labels_task = {
-        "cola": 12,
+        "cola": 18,
         "sst-2": 2,
         "mnli": 3,
         "mrpc": 2,
